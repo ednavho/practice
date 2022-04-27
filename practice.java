3db79e897d3e676d169c9e5ee7f8ecc020904426
ednavho <ednaho121@gmail.com>
2022-04-27 14:00:17 -0700

    added test file

--- practice.java
+++ practice.java
@@ -1,1 +1,6 @@
+public class practice {
 
+    public static int multiply(int n, int m) {
+        return n * m;
+    }
+}
---
+++ practiceTester.java
@@ -0,0 +1,12 @@
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class practiceTester {
+    
+    @Test
+    public void multiplication() {
+        assertEquals(8, practice.multiply(4,3));
+    }
+}
